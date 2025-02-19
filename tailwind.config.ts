351022a6d8ddf69e31cf4284714dddbdc3c4c9e2
danny boy <danny.boy.23.pub@gmail.com>
2025-02-19 12:35:07 -0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'polaroid': "url('/main.jpg')",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
@@ -15,4 +18,5 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+}
+satisfies Config;
